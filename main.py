a51a6b9359589d63c13d14aa44419a0b7e627ccc
IrekTursunov <tursunov.irek@gmail.com>
2022-01-23 21:54:49 +0500

    корректировка #3

--- main.py
+++ main.py
@@ -11,11 +11,11 @@
 #
 # print(f"По опросам пользователей средняя оценка равна {(mark1+mark2+mark3+mark4)/count_users}.")
 marks = '1231423412345312444555'
-count_one   = marks.count('1')
-count_two   = marks.count('2')
+count_one = marks.count('1')
+count_two = marks.count('2')
 count_three = marks.count('3')
-count_four  = marks.count('4')
-count_five  = marks.count('5')
-average_mark = (count_one+count_two*2+count_three*3+count_four*4+count_five*5)/len(marks)
+count_four = marks.count('4')
+count_five = marks.count('5')
+average_mark = (count_one + count_two * 2 + count_three * 3 + count_four * 4 + count_five * 5) / len(marks)
 print(f'Средняя оценка пользователей:  {average_mark}')
 print(f'Приближенная средняя оценка пользователей:  {round(average_mark)}')
